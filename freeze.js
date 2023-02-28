b767b3f53e5b6c6c3282e786c5290053aff2d040
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 23:35:27 +0600

    added object delet ,seel freez

--- freeze.js
+++ freeze.js
@@ -9,4 +9,15 @@ const keys=Object.keys(bottle);
 const values=Object.values(bottle);
 // console.log(values);
 const pair=Object.entries(bottle)
-console.log(pair);
+// console.log(pair);
+
+
+delete bottle.isClean;
+console.log(bottle);
+
+Object.seal(bottle)
+delete bottle.color;
+console.log(bottle);
+bottle.price=400;
+console.log(bottle);
+Object.freeze(bottle)
