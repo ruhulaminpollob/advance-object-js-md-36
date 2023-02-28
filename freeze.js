f99d73e0afb7bb109c5ae3e286de4e30f1847fe7
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 23:29:10 +0600

    get pair keys and values  from object

--- freeze.js
+++ freeze.js
@@ -7,4 +7,6 @@ const bottle={
 const keys=Object.keys(bottle);
 // console.log(keys);
 const values=Object.values(bottle);
-console.log(values);
+// console.log(values);
+const pair=Object.entries(bottle)
+console.log(pair);
