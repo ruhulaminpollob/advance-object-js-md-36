504382ffd3dc11008e790cd365f508169bdf6374
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 23:18:34 +0600

    change object value by using inner object function

--- methods.js
+++ methods.js
@@ -1,6 +1,7 @@
 const student={
     name:'Rakib Khan',
     study:'Math',
+    money:5000,
     subjects:['calculus', 'algebra', 'geometry'],
     exam:function () {
         return this.name+" is participate in exam";
@@ -9,8 +10,15 @@ const student={
         this.exam()
         return `${this.name} need improvement exam in ${subject}`
     },
+    treetDay:function (amount) {
+        this.money=this.money-amount;
+        return this.money
+    }
 }
 // console.log(student);
-console.log(student.exam());
+// console.log(student.exam());
 const retest=student.improve('algebra');
-console.log(retest);
+// console.log(retest);
+
+const remaining=student.treetDay(2999)
+console.log(remaining);
