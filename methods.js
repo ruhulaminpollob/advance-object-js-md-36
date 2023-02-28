6fbe79da0eaccd07c9a94f7e36811afcdaae0ad8
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 23:13:05 +0600

    added inner object functions

--- methods.js
+++ methods.js
@@ -3,6 +3,14 @@ const student={
     study:'Math',
     subjects:['calculus', 'algebra', 'geometry'],
     exam:function () {
-        console.log(this.name , "is participate in exam");
-    }
+        return this.name+" is participate in exam";
+    },
+    improve:function (subject) {
+        this.exam()
+        return `${this.name} need improvement exam in ${subject}`
+    },
 }
+// console.log(student);
+console.log(student.exam());
+const retest=student.improve('algebra');
+console.log(retest);
