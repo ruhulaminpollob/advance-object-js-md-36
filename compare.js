73ff3d5d604cc8fe9cc58dfadf6ef7f66ea237e3
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-01 00:26:24 +0600

    added compareson method

--- compare.js
+++ compare.js
@@ -17,3 +17,27 @@ if (fastString===secondString) {
 } else {
     console.log('different');
 }
+
+
+
+const num1={a:3,b:6,c:9, d:9};
+const num2={b:6,c:9,a:3};
+
+const isEqual=(num1,num2)=>{
+    const num1Keys=Object.keys(num1);
+    const num2Keys=Object.keys(num2);
+    if (num1Keys.length===num2Keys.length) {
+        for (const keys of num1Keys) {
+            if (num1Keys[keys] !== num2Keys[keys]) {
+                return false;
+            }
+            return true;
+        }
+    } else {
+        return false;
+    }
+
+}
+
+const compare=isEqual(num1,num2)
+console.log(compare);
