65fecc01e95d423416383f8447f7900548896d6f
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 23:51:21 +0600

    added for in loop

---
+++ forInLoop.js
@@ -0,0 +1,17 @@
+const bottle={
+    color:'green',
+    isClean:true,
+    price:'200 taka',
+    capacity:2,
+}
+
+
+
+console.log(bottle);
+
+for (const key in bottle) {
+    console.log(key, bottle[key]);
+        
+}
+
+// console.log(bottle['color']);
