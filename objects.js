b53e722239e90b8e08d8a4e2e3871d37ba6a3199
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 22:08:31 +0600

    added pbject prorerty and object

---
+++ objects.js
@@ -0,0 +1,12 @@
+// create object using object  literals
+
+const player={};
+player.name='Shakib Al Hassan';
+player.specialty='batsman';
+
+player.bat=function(){
+    console.log('swing his bat');
+}
+
+console.log(player);
+player.bat()
